c524026301147020388cadd6e21041aaf624d114
Sargun Dhillon <sargun@sargun.me>
2015-12-08 20:58:10 -0800

    Initial commit

---
+++ apps/dcos_l4lb/src/dcos_l4lb_app.erl
@@ -0,0 +1,16 @@
+-module(dcos_l4lb_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    dcos_l4lb_sup:start_link().
+
+stop(_State) ->
+    ok.
