66de6f7b6698085bf6b777360b608c4e23cfb810
Sargun Dhillon <sargun@sargun.me>
2015-12-16 11:08:39 -0800

    First go at the multiqueue work

--- apps/dcos_l4lb/src/dcos_l4lb_network_sup.erl
+++ apps/dcos_l4lb/src/dcos_l4lb_network_sup.erl
@@ -11,31 +11,32 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Num), {I, {I, start_link, [Num]}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
-start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Num) ->
+  supervisor:start_link(?MODULE, [Num]).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
+init([Num]) ->
   {ok,
    { {one_for_one, 5, 10},
      [
-       ?CHILD(dcos_l4lb_routes, worker),
-       ?CHILD(dcos_l4lb_ct, worker),
-       ?CHILD(dcos_l4lb_packet_handler, worker),
-       ?CHILD(dcos_l4lb_nfq, worker)
+       ?CHILD(dcos_l4lb_ct, worker, Num),
+       ?CHILD(dcos_l4lb_packet_handler, worker, Num),
+       ?CHILD(dcos_l4lb_nfq, worker, Num)
      ]} }.
+
+
