03b4544cf7e945c1046922340f773c380bd0597e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-02-19 15:29:36 -0800

    Add conversion of questions.

--- apps/dcos_dns/apps/spartan/src/dcos_dns_dns_dual_dispatch_fsm.erl
+++ apps/dcos_dns/apps/spartan/src/dcos_dns_dns_dual_dispatch_fsm.erl
@@ -221,14 +221,110 @@ convert(Message) ->
               authority = convert(nslist, Authorities),
               additional = convert(arlist, Resources)}.
 
-convert(qdlist, _Questions) ->
-    [];
+%% [{dns_query,"google.com",a,in}] -> [{dns_query,<<"2.zk">>,1,1}]
+convert(qdlist, Questions) ->
+    [convert(qd, Question) || Question <- Questions];
 convert(anlist, _Answers) ->
     [];
 convert(nslist, _Authorities) ->
     [];
 convert(arlist, _Resources) ->
-    [].
+    [];
+
+convert(qd, Question) ->
+    Domain = inet_dns:dns_query(Question, domain),
+    Type = inet_dns:dns_query(Question, type),
+    Class = inet_dns:dns_query(Question, class),
+    #dns_query{name = list_to_binary(Domain),
+               class = class_to_integer(list_to_binary(upcase(atom_to_list(Class)))),
+               type = type_to_integer(list_to_binary(upcase(atom_to_list(Type))))}.
+
+class_to_integer(Bin) when is_binary(Bin) ->
+    case Bin of
+        ?DNS_CLASS_IN_BSTR -> ?DNS_CLASS_IN_NUMBER;
+        ?DNS_CLASS_CS_BSTR -> ?DNS_CLASS_CS_NUMBER;
+        ?DNS_CLASS_CH_BSTR -> ?DNS_CLASS_CH_NUMBER;
+        ?DNS_CLASS_HS_BSTR -> ?DNS_CLASS_HS_NUMBER;
+        ?DNS_CLASS_NONE_BSTR -> ?DNS_CLASS_NONE_NUMBER;
+        ?DNS_CLASS_ANY_BSTR -> ?DNS_CLASS_ANY_NUMBER;
+        _ -> undefined
+    end.
+
+type_to_integer(Bin) when is_binary(Bin) ->
+    case Bin of
+        ?DNS_TYPE_A_BSTR -> ?DNS_TYPE_A_NUMBER;
+        ?DNS_TYPE_NS_BSTR -> ?DNS_TYPE_NS_NUMBER;
+        ?DNS_TYPE_MD_BSTR -> ?DNS_TYPE_MD_NUMBER;
+        ?DNS_TYPE_MF_BSTR -> ?DNS_TYPE_MF_NUMBER;
+        ?DNS_TYPE_CNAME_BSTR -> ?DNS_TYPE_CNAME_NUMBER;
+        ?DNS_TYPE_SOA_BSTR -> ?DNS_TYPE_SOA_NUMBER;
+        ?DNS_TYPE_MB_BSTR -> ?DNS_TYPE_MB_NUMBER;
+        ?DNS_TYPE_MG_BSTR -> ?DNS_TYPE_MG_NUMBER;
+        ?DNS_TYPE_MR_BSTR -> ?DNS_TYPE_MR_NUMBER;
+        ?DNS_TYPE_NULL_BSTR -> ?DNS_TYPE_NULL_NUMBER;
+        ?DNS_TYPE_WKS_BSTR -> ?DNS_TYPE_WKS_NUMBER;
+        ?DNS_TYPE_PTR_BSTR -> ?DNS_TYPE_PTR_NUMBER;
+        ?DNS_TYPE_HINFO_BSTR -> ?DNS_TYPE_HINFO_NUMBER;
+        ?DNS_TYPE_MINFO_BSTR -> ?DNS_TYPE_MINFO_NUMBER;
+        ?DNS_TYPE_MX_BSTR -> ?DNS_TYPE_MX_NUMBER;
+        ?DNS_TYPE_TXT_BSTR -> ?DNS_TYPE_TXT_NUMBER;
+        ?DNS_TYPE_RP_BSTR -> ?DNS_TYPE_RP_NUMBER;
+        ?DNS_TYPE_AFSDB_BSTR -> ?DNS_TYPE_AFSDB_NUMBER;
+        ?DNS_TYPE_X25_BSTR -> ?DNS_TYPE_X25_NUMBER;
+        ?DNS_TYPE_ISDN_BSTR -> ?DNS_TYPE_ISDN_NUMBER;
+        ?DNS_TYPE_RT_BSTR -> ?DNS_TYPE_RT_NUMBER;
+        ?DNS_TYPE_NSAP_BSTR -> ?DNS_TYPE_NSAP_NUMBER;
+        ?DNS_TYPE_SIG_BSTR -> ?DNS_TYPE_SIG_NUMBER;
+        ?DNS_TYPE_KEY_BSTR -> ?DNS_TYPE_KEY_NUMBER;
+        ?DNS_TYPE_PX_BSTR -> ?DNS_TYPE_PX_NUMBER;
+        ?DNS_TYPE_GPOS_BSTR -> ?DNS_TYPE_GPOS_NUMBER;
+        ?DNS_TYPE_AAAA_BSTR -> ?DNS_TYPE_AAAA_NUMBER;
+        ?DNS_TYPE_LOC_BSTR -> ?DNS_TYPE_LOC_NUMBER;
+        ?DNS_TYPE_NXT_BSTR -> ?DNS_TYPE_NXT_NUMBER;
+        ?DNS_TYPE_EID_BSTR -> ?DNS_TYPE_EID_NUMBER;
+        ?DNS_TYPE_NIMLOC_BSTR -> ?DNS_TYPE_NIMLOC_NUMBER;
+        ?DNS_TYPE_SRV_BSTR -> ?DNS_TYPE_SRV_NUMBER;
+        ?DNS_TYPE_ATMA_BSTR -> ?DNS_TYPE_ATMA_NUMBER;
+        ?DNS_TYPE_NAPTR_BSTR -> ?DNS_TYPE_NAPTR_NUMBER;
+        ?DNS_TYPE_KX_BSTR -> ?DNS_TYPE_KX_NUMBER;
+        ?DNS_TYPE_CERT_BSTR -> ?DNS_TYPE_CERT_NUMBER;
+        ?DNS_TYPE_DNAME_BSTR -> ?DNS_TYPE_DNAME_NUMBER;
+        ?DNS_TYPE_SINK_BSTR -> ?DNS_TYPE_SINK_NUMBER;
+        ?DNS_TYPE_OPT_BSTR -> ?DNS_TYPE_OPT_NUMBER;
+        ?DNS_TYPE_APL_BSTR -> ?DNS_TYPE_APL_NUMBER;
+        ?DNS_TYPE_DS_BSTR -> ?DNS_TYPE_DS_NUMBER;
+        ?DNS_TYPE_SSHFP_BSTR -> ?DNS_TYPE_SSHFP_NUMBER;
+        ?DNS_TYPE_IPSECKEY_BSTR -> ?DNS_TYPE_IPSECKEY_NUMBER;
+        ?DNS_TYPE_RRSIG_BSTR -> ?DNS_TYPE_RRSIG_NUMBER;
+        ?DNS_TYPE_NSEC_BSTR -> ?DNS_TYPE_NSEC_NUMBER;
+        ?DNS_TYPE_DNSKEY_BSTR -> ?DNS_TYPE_DNSKEY_NUMBER;
+        ?DNS_TYPE_NSEC3_BSTR -> ?DNS_TYPE_NSEC3_NUMBER;
+        ?DNS_TYPE_NSEC3PARAM_BSTR -> ?DNS_TYPE_NSEC3PARAM_NUMBER;
+        ?DNS_TYPE_DHCID_BSTR -> ?DNS_TYPE_DHCID_NUMBER;
+        ?DNS_TYPE_HIP_BSTR -> ?DNS_TYPE_HIP_NUMBER;
+        ?DNS_TYPE_NINFO_BSTR -> ?DNS_TYPE_NINFO_NUMBER;
+        ?DNS_TYPE_RKEY_BSTR -> ?DNS_TYPE_RKEY_NUMBER;
+        ?DNS_TYPE_TALINK_BSTR -> ?DNS_TYPE_TALINK_NUMBER;
+        ?DNS_TYPE_SPF_BSTR -> ?DNS_TYPE_SPF_NUMBER;
+        ?DNS_TYPE_UINFO_BSTR -> ?DNS_TYPE_UINFO_NUMBER;
+        ?DNS_TYPE_UID_BSTR -> ?DNS_TYPE_UID_NUMBER;
+        ?DNS_TYPE_GID_BSTR -> ?DNS_TYPE_GID_NUMBER;
+        ?DNS_TYPE_UNSPEC_BSTR -> ?DNS_TYPE_UNSPEC_NUMBER;
+        ?DNS_TYPE_TKEY_BSTR -> ?DNS_TYPE_TKEY_NUMBER;
+        ?DNS_TYPE_TSIG_BSTR -> ?DNS_TYPE_TSIG_NUMBER;
+        ?DNS_TYPE_IXFR_BSTR -> ?DNS_TYPE_IXFR_NUMBER;
+        ?DNS_TYPE_AXFR_BSTR -> ?DNS_TYPE_AXFR_NUMBER;
+        ?DNS_TYPE_MAILB_BSTR -> ?DNS_TYPE_MAILB_NUMBER;
+        ?DNS_TYPE_MAILA_BSTR -> ?DNS_TYPE_MAILA_NUMBER;
+        ?DNS_TYPE_ANY_BSTR -> ?DNS_TYPE_ANY_NUMBER;
+        ?DNS_TYPE_DLV_BSTR -> ?DNS_TYPE_DLV_NUMBER;
+        _ -> undefined
+    end.
+
+%% @private
+upcase(Y) ->
+    Upcase = fun(X) when $a =< X,  X =< $z -> X + $A - $a; (X) -> X end,
+    lists:map(Upcase, Y).
 
 -ifdef(TEST).
 
