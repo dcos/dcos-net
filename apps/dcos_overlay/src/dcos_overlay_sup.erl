0b3e30d1d21151a38abece83a39171663fd8dc99
Sargun Dhillon <sargun@sargun.me>
2016-05-27 17:17:22 -0700

    Add first version of navstar overlay

---
+++ apps/dcos_overlay/src/dcos_overlay_sup.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%% @doc navstar top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(dcos_overlay_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+
+%% API
+-export([]).
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, {{one_for_all, 5, 10}, [
+    ]}}.
