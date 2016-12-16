700e2aebb4d2264937d0482fb031cad0c3f60cbd
Sargun Dhillon <sargun@sargun.me>
2016-12-16 12:17:24 -0800

    Merge Minuteman into navstar

--- apps/dcos_net/src/dcos_net_app.erl
+++ apps/dcos_net/src/dcos_net_app.erl
@@ -23,6 +23,7 @@
 start(_StartType, _StartArgs) ->
     load_config(),
     maybe_add_master(),
+    maybe_start_minuteman(),
     'dcos_net_sup':start_link().
 
 %%--------------------------------------------------------------------
@@ -32,6 +33,14 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+maybe_start_minuteman() ->
+    case application:get_env(navstar, enable_lb, false) of
+        false ->
+            ok;
+        true ->
+            {ok, _} = application:ensure_all_started(minuteman)
+    end.
 
 maybe_add_master() ->
     case application:get_env(navstar, is_master, false) of
