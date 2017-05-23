cbce00a203e394665c1ceca2c0b5d23c67e2be29
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2017-05-23 11:49:43 -0700

    Remove navstar_overlay_mockhttp_backend module

--- apps/dcos_overlay/test/dcos_overlay_mockhttp_frontend.erl
+++ apps/dcos_overlay/test/dcos_overlay_mockhttp_frontend.erl
@@ -1,6 +1,7 @@
 -module(dcos_overlay_mockhttp_frontend).
 
 -include_lib("common_test/include/ct.hrl").
+-include_lib("mesos_state/include/mesos_state_overlay_pb.hrl").
 
 % To mock cowboy
 -export([init/2, content_types_provided/2,
@@ -17,5 +18,29 @@ allowed_methods(Req, Opts) ->
 
 to_protobuf(Req, Opts = [overlay]) ->
   Node = cowboy_req:header(<<"node">>, Req),
-  {ok, Data} = gen_server:call('dcos_overlay_mockhttp_backend', {data, Node}),
+  Data = create_data(Node),
   {mesos_state_overlay_pb:encode_msg(Data), Req, Opts}.
+
+parse_node(Agent) ->
+  [Bin1, _ ] = binary:split(Agent, <<"@">>),
+  [_, Num] = binary:split(Bin1, [<<"master">>,<<"agent">>]),
+  binary_to_list(Num).
+
+create_data(Agent) ->
+    NodeNumber = parse_node(Agent),
+    HexNodeNumber = list_to_integer(NodeNumber, 16),
+    Info = #mesos_state_overlayinfo{name = <<"dcos">>, prefix = 24, subnet = <<"9.0.0.0/8">>},
+    MesosIP = list_to_binary(io_lib:format("9.0.~s.0/25", [NodeNumber])),
+    MesosBridge = #mesos_state_bridgeinfo{name = <<"m-dcos">>, ip = MesosIP},
+    DockerIP = list_to_binary(io_lib:format("9.0.~s.128/25", [NodeNumber])),
+    DockerBridge = #mesos_state_bridgeinfo{name = <<"d-dcos">>, ip = DockerIP},
+    Vtep_ip = list_to_binary(io_lib:format("44.128.0.~s/20", [NodeNumber])),
+    Vtep_mac = list_to_binary(io_lib:format("70:b3:d5:80:00:~p", [HexNodeNumber])),
+    Vxlan = #mesos_state_vxlaninfo{vni = 1024, vtep_ip = Vtep_ip, vtep_mac = Vtep_mac, vtep_name = <<"vtep1024">>},
+    Backend = #mesos_state_backendinfo{vxlan = Vxlan},
+    OverlayState = #'mesos_state_agentoverlayinfo.state'{status = 'STATUS_OK'},
+    Subnet = list_to_binary(io_lib:format("9.0.~s.0/24", [NodeNumber])),
+    Overlay = #mesos_state_agentoverlayinfo{info = Info, subnet = Subnet, backend = Backend,
+                  mesos_bridge = MesosBridge, docker_bridge = DockerBridge, state = OverlayState},
+    AgentIP = list_to_binary(io_lib:format("10.0.0.~s",[NodeNumber])),
+    #mesos_state_agentinfo{ip = AgentIP, overlays = [Overlay]}.
