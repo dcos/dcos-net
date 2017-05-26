033e9f084fab9d8c9ead51454f82973882e949df
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2017-05-25 17:30:22 -0700

    Prettify spartan_udp_server

--- apps/dcos_dns/src/dcos_dns_udp_server.erl
+++ apps/dcos_dns/src/dcos_dns_udp_server.erl
@@ -1,25 +1,27 @@
 -module(dcos_dns_udp_server).
--author("sdhillon").
-
 -behaviour(gen_server).
 
 %% API
--export([start_link/1,
-         do_reply/2]).
+-export([
+    start_link/1,
+    do_reply/2
+]).
 
 %% gen_server callbacks
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2,
+    terminate/2,
+    code_change/3
+]).
 
 -include("dcos_dns.hrl").
 
 -record(state, {
-    port = erlang:error() :: inet:port_number(),
-    socket = erlang:error() :: gen_udp:socket()
+    port :: inet:port_number(),
+    socket :: gen_udp:socket()
 }).
 
 %%%===================================================================
@@ -31,12 +33,6 @@ do_reply(To = {Pid, {IP, Port}}, Data) ->
     lager:debug("Doing reply to ~p: ~p", [To, Data]),
     gen_server:cast(Pid, {send_behalf, IP, Port, Data}).
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
 -spec(start_link(LocalIP :: inet:ip4_address()) ->
     {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link(LocalIP) ->
@@ -45,6 +41,7 @@ start_link(LocalIP) ->
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
+
 init([LocalIP]) ->
     Port = dcos_dns_config:udp_port(),
     RecBuf = application:get_env(dcos_dns, udp_recbuf, 1024 * 1024),
@@ -75,8 +72,7 @@ handle_info({udp, RecvSocket, FromIP, FromPort, Data},
             error
     end,
     {noreply, State};
-handle_info(Info, State) ->
-    lager:debug("Received info: ~p", [Info]),
+handle_info(_Info, State) ->
     {noreply, State}.
 
 terminate(_Reason, _State = #state{socket = Socket}) ->
@@ -85,8 +81,3 @@ terminate(_Reason, _State = #state{socket = Socket}) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-
