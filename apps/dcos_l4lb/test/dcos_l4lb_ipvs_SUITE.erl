f151f41a17b8eb699b334fb2bf4278482d19ca30
Deepak Goel <dgoel@mesosphere.com>
2017-10-23 18:29:51 -0700

    fix minuteman routes for ipv6

--- apps/dcos_l4lb/test/dcos_l4lb_ipvs_SUITE.erl
+++ apps/dcos_l4lb/test/dcos_l4lb_ipvs_SUITE.erl
@@ -30,11 +30,15 @@ all(_, _) -> [].
 init_per_testcase(_, Config) ->
     case os:cmd("ipvsadm -C") of
         "" ->
+            AgentIP = {2, 2, 2, 2},
             os:cmd("ip link del minuteman"),
             os:cmd("ip link add minuteman type dummy"),
+            os:cmd("ip link set minuteman up"),
             os:cmd("ip link del webserver"),
             os:cmd("ip link add webserver type dummy"),
             os:cmd("ip link set webserver up"),
+            os:cmd(lists:flatten(io_lib:format(
+                   "ip addr add ~s/32 dev webserver", [inet:ntoa(AgentIP)]))), 
             os:cmd("ip addr add 1.1.1.1/32 dev webserver"),
             os:cmd("ip addr add 1.1.1.2/32 dev webserver"),
             os:cmd("ip addr add 1.1.1.3/32 dev webserver"),
@@ -43,7 +47,7 @@ init_per_testcase(_, Config) ->
             application:set_env(dcos_l4lb, enable_networking, true),
             {ok, _} = application:ensure_all_started(inets),
             {ok, _} = application:ensure_all_started(dcos_l4lb),
-            Config;
+            [{agentip, AgentIP} | Config];
         Result ->
             {skip, Result}
     end.
@@ -61,18 +65,19 @@ end_per_testcase(_, _Config) ->
     ok.
 
 make_v4_webserver(Idx) ->
-    make_webserver({1, 1, 1, Idx}).
+    make_webserver(inet, {1, 1, 1, Idx}).
 
 make_v6_webserver() ->
-    make_webserver({16#fc01, 16#0, 16#0, 16#0, 16#0, 16#0, 16#0, 16#1}).
-    
-make_webserver(IP) ->
+    make_webserver(inet6, {16#fc01, 16#0, 16#0, 16#0, 16#0, 16#0, 16#0, 16#1}).
+
+make_webserver(Family, IP) ->
     file:make_dir("/tmp/htdocs"),
     {ok, Pid} = inets:start(httpd, [
         {port, 0},
         {server_name, "httpd_test"},
         {server_root, "/tmp"},
         {document_root, "/tmp/htdocs"},
+        {ipfamily, Family},
         {bind_address, IP}
     ]),
     Pid.
@@ -80,19 +85,19 @@ make_webserver(IP) ->
 %webservers() ->
 %    lists:map(fun make_webserver/1, lists:seq(1,3)).
 
-webserver(Pid) ->
+webserver(Pid, AgentIP) ->
     Info = httpd:info(Pid),
     Port = proplists:get_value(port, Info),
     IP = proplists:get_value(bind_address, Info),
-    {IP, Port}.
+    {AgentIP, {IP, Port}}.
 
-add_webserver(VIP, {IP, Port}) ->
+add_webserver(VIP, WebServer) ->
     % inject an update for this vip
-    {ok, _} = lashup_kv:request_op(?VIPS_KEY2, {update, [{update, {VIP, riak_dt_orswot}, {add, {IP, {IP, Port}}}}]}).
+    {ok, _} = lashup_kv:request_op(?VIPS_KEY2, {update, [{update, {VIP, riak_dt_orswot}, {add, WebServer}}]}).
 
-remove_webserver(VIP, {IP, Port}) ->
+remove_webserver(VIP, WebServer) ->
     % inject an update for this vip
-    {ok, _} = lashup_kv:request_op(?VIPS_KEY2, {update, [{update, {VIP, riak_dt_orswot}, {remove, {IP, {IP, Port}}}}]}).
+    {ok, _} = lashup_kv:request_op(?VIPS_KEY2, {update, [{update, {VIP, riak_dt_orswot}, {remove, WebServer}}]}).
 
 remove_vip(VIP) ->
     {ok, _} = lashup_kv:request_op(?VIPS_KEY2, {update, [{remove, {VIP, riak_dt_orswot}}]}).
@@ -108,11 +113,12 @@ test_vip(0, _, _) ->
 test_vip(Tries, Family, VIP = {tcp, IP0, Port}) ->
     IP1 = inet:ntoa(IP0),
     URI = uri(Family, IP1, Port),
+    httpc:set_options([{ipfamily, Family}]),
     case httpc:request(get, {URI, _Headers = []}, [{timeout, 500}, {connect_timeout, 500}], []) of
         {ok, _} ->
             ok;
         {error, _} ->
-            test_vip(Tries - 1, VIP)
+            test_vip(Tries - 1, Family, VIP)
     end.
 
 uri(inet, IP, Port) ->
@@ -120,31 +126,31 @@ uri(inet, IP, Port) ->
 uri(inet6, IP, Port) ->
     lists:flatten(io_lib:format("http://[~s]:~b/", [IP, Port])).
 
-test_v4(_Config) ->
+test_v4(Config) ->
+    AgentIP = ?config(agentip, Config), 
     Family = inet,
-    timer:sleep(1000),
     W1 = make_v4_webserver(1),
     W2 = make_v4_webserver(2),
     VIP = {tcp, {11, 0, 0, 1}, 8080},
-    add_webserver(VIP, webserver(W1)),
+    add_webserver(VIP, webserver(W1, AgentIP)),
     ok = test_vip(Family, VIP),
-    remove_webserver(VIP, webserver(W1)),
+    remove_webserver(VIP, webserver(W1, AgentIP)),
     inets:stop(stand_alone, W1),
-    add_webserver(VIP, webserver(W2)),
+    add_webserver(VIP, webserver(W2, AgentIP)),
     ok = test_vip(Family, VIP),
-    remove_webserver(VIP, webserver(W2)),
+    remove_webserver(VIP, webserver(W2, AgentIP)),
     error = test_vip(Family, VIP),
     remove_vip(VIP),
     error = test_vip(Family, VIP).
 
-test_v6(_Config) ->
+test_v6(Config) ->
+    AgentIP = ?config(agentip, Config),
     Family = inet6,
-    timer:sleep(1000),
     W = make_v6_webserver(),
     VIP = {tcp, {16#fd01, 16#0, 16#0, 16#0, 16#0, 16#0, 16#0, 16#1}, 8080},
-    add_webserver(VIP, webserver(W)),
+    add_webserver(VIP, webserver(W, AgentIP)),
     ok = test_vip(Family, VIP),
-    remove_webserver(VIP, webserver(W)),
+    remove_webserver(VIP, webserver(W, AgentIP)),
     error = test_vip(Family, VIP),
     remove_vip(VIP),
     error = test_vip(Family, VIP).
