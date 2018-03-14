9949f1e082e2b973f7ced4c8ba81ee4654570f8a
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-03-14 16:27:16 -0700

    Tasks on host network don't have container ip

--- apps/dcos_dns/src/dcos_dns_mesos.erl
+++ apps/dcos_dns/src/dcos_dns_mesos.erl
@@ -130,6 +130,8 @@ task_agentip(#{name := Name, framework := Fwrk, agent_ip := AgentIP}) ->
     dns_records(DName, [AgentIP]).
 
 -spec(task_containerip(task()) -> [dns:dns_rr()]).
+task_containerip(#{agent_ip := AgentIP, task_ip := [AgentIP]}) ->
+    [];
 task_containerip(#{name := Name, framework := Fwrk,
                    task_ip := TaskIPs}) ->
     DName = format_name([Name, Fwrk, <<"containerip">>], ?DCOS_DOMAIN),
