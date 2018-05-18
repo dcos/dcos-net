a369a366cf8bca252a30d773edb92f0486e4d9a3
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-05-18 15:27:00 -0700

    Add REST metrics

--- apps/dcos_rest/src/dcos_rest_sup.erl
+++ apps/dcos_rest/src/dcos_rest_sup.erl
@@ -12,6 +12,8 @@ init([true]) ->
     {ok, {#{}, []}}.
 
 setup_cowboy() ->
+    ok = init_metrics(),
+
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/lashup/kv/[...]", dcos_rest_lashup_handler, []},
@@ -28,7 +30,33 @@ setup_cowboy() ->
     ]),
     {ok, Ip} = application:get_env(dcos_rest, ip),
     {ok, Port} = application:get_env(dcos_rest, port),
+    StreamHandlers = [cowboy_metrics_h, cowboy_stream_h],
+
     {ok, _} = cowboy:start_clear(
         http, [{ip, Ip}, {port, Port}], #{
-            env => #{dispatch => Dispatch}
+            env => #{dispatch => Dispatch},
+            stream_handlers => StreamHandlers,
+            metrics_callback => fun metrics_fun/1
         }).
+
+%%%===================================================================
+%%% Metrics functions
+%%%===================================================================
+
+-spec(metrics_fun(cowboy_metrics_h:metrics()) -> any()).
+metrics_fun(#{req_start := ReqStart, req_end := ReqEnd,
+              resp_status := Status}) ->
+    ok = folsom_metrics:notify(
+        {rest, response_ms},
+        (ReqEnd - ReqStart) div 1000 / 1000),
+    ok = folsom_metrics:notify({rest, requests}, {inc, 1}, counter),
+    ok = folsom_metrics:notify({rest, statuses, Status}, {inc, 1}, counter).
+
+-define(SLIDE_WINDOW, 5). % seconds
+-define(SLIDE_SIZE, 1024).
+
+-spec(init_metrics() -> ok).
+init_metrics() ->
+    folsom_metrics:new_histogram(
+        {rest, response_ms}, slide_uniform,
+        {?SLIDE_WINDOW, ?SLIDE_SIZE}).
