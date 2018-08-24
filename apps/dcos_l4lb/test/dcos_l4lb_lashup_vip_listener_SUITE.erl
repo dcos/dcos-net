30046aaf541225bbb817bc3e50eca557cd1f281c
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-08-24 11:14:14 -0700

    Code refactoring

--- apps/dcos_l4lb/test/dcos_l4lb_lashup_vip_listener_SUITE.erl
+++ apps/dcos_l4lb/test/dcos_l4lb_lashup_vip_listener_SUITE.erl
@@ -1,27 +1,29 @@
 -module(dcos_l4lb_lashup_vip_listener_SUITE).
 
 -include_lib("common_test/include/ct.hrl").
+-include_lib("dns/include/dns.hrl").
 -include("dcos_l4lb.hrl").
 
 -export([
+    name2ip/1,
     all/0,
     init_per_suite/1, end_per_suite/1,
     init_per_testcase/2, end_per_testcase/2,
-    test_uninitalized_table/1,
-    lookup_vip/1,
-    lookup_failure/1,
-    lookup_failure2/1,
-    lookup_failure3/1
+    lookup_vips/1
 ]).
 
+name2ip(DName) ->
+    name2ip(?DNS_TYPE_A, DName).
 
-%% root tests
+name2ip(DType, DName) ->
+    case resolve(DType, <<DName/binary, ".l4lb.thisdcos.directory">>) of
+        [#dns_rr{data=#dns_rrdata_a{ip = IP}}] -> IP;
+        [#dns_rr{data=#dns_rrdata_aaaa{ip = IP}}] -> IP;
+        [] -> false
+    end.
+
 all() ->
-  [test_uninitalized_table,
-   lookup_vip,
-   lookup_failure,
-   lookup_failure2,
-   lookup_failure3].
+    [lookup_vips].
 
 init_per_suite(Config) ->
     Config.
@@ -29,12 +31,10 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     Config.
 
-init_per_testcase(test_uninitalized_table, Config) -> Config;
 init_per_testcase(_, Config) ->
     {ok, _} = application:ensure_all_started(dcos_l4lb),
     Config.
 
-end_per_testcase(test_uninitalized_table, _Config) -> ok;
 end_per_testcase(_, _Config) ->
     [ begin
         ok = application:stop(App),
@@ -44,40 +44,49 @@ end_per_testcase(_, _Config) ->
     os:cmd("rm -rf Mnesia.*"),
     ok.
 
-test_uninitalized_table(_Config) ->
-  IP = {10, 0, 1, 10},
-  [] = dcos_l4lb_lashup_vip_listener:lookup_vips([{ip, IP}]),
-  ok.
+-define(LKEY(K), {{tcp, K, 80}, riak_dt_orswot}).
+-define(LKEY(L, F), ?LKEY({name, {L, F}})).
+-define(BE4, {{1, 2, 3, 4}, {{1, 2, 3, 4}, 80}}).
+-define(BE6, {{1, 2, 3, 4}, {{1, 0, 0, 0, 0, 0, 0, 1}, 80}}).
 
-lookup_failure(_Config) ->
-  IP = {10, 0, 1, 10},
-  [{badmatch, IP}] = dcos_l4lb_lashup_vip_listener:lookup_vips([{ip, IP}]),
-  Name = <<"foobar.marathon">>,
-  [{badmatch, Name}] = dcos_l4lb_lashup_vip_listener:lookup_vips([{name, Name}]),
-  ok.
+lookup_vips(_Config) ->
+    lashup_kv:request_op(?VIPS_KEY2, {update, [
+        {update, ?LKEY({1, 2, 3, 4}), {add, ?BE4}},
+        {update, ?LKEY(<<"/foo">>, <<"bar">>), {add, ?BE4}},
+        {update, ?LKEY(<<"/baz">>, <<"qux">>), {add, ?BE4}},
+        {update, ?LKEY(<<"/qux">>, <<"ipv6">>), {add, ?BE6}}
+    ]}),
+    timer:sleep(100),
+    {11, 0, 0, 37} = name2ip(<<"foo.bar">>),
+    {11, 0, 0, 39} = IP4 = name2ip(<<"baz.qux">>),
+    {16#fd01, 0, 0, 0, 0, 0, 0, 1} = IP6 =
+        name2ip(?DNS_TYPE_AAAA, <<"qux.ipv6">>),
 
-lookup_failure2(Config) ->
-  {ok, _} = lashup_kv:request_op(?VIPS_KEY2, {update, [{update,
-                                                       {{tcp, {1, 2, 3, 4}, 5000}, riak_dt_orswot},
-                                                       {add, {{10, 0, 1, 10}, {{10, 0, 1, 10}, 17780}}}}]}),
-  lookup_failure(Config),
-  ok.
+    lashup_kv:request_op(?VIPS_KEY2, {update, [
+        {update, ?LKEY(<<"/foo">>, <<"bar">>), {remove, ?BE4}}
+    ]}),
+    timer:sleep(100),
+    false = name2ip(<<"foo.bar">>),
+    IP4 = name2ip(<<"baz.qux">>),
+    IP6 = name2ip(?DNS_TYPE_AAAA, <<"qux.ipv6">>),
 
-lookup_failure3(Config) ->
-  {ok, _} = update_de8b9dc86_marathon(),
-  lookup_failure(Config),
-  ok.
+    lashup_kv:request_op(?VIPS_KEY2, {update, [
+        {update, ?LKEY({1, 2, 3, 4}), {remove, ?BE4}},
+        {update, ?LKEY(<<"/baz">>, <<"qux">>), {remove, ?BE4}},
+        {update, ?LKEY(<<"/qux">>, <<"ipv6">>), {remove, ?BE6}}
+    ]}),
+    timer:sleep(100),
+    false = name2ip(<<"foo.bar">>),
+    false = name2ip(<<"baz.qux">>),
+    false = name2ip(?DNS_TYPE_AAAA, <<"qux.ipv6">>).
 
-lookup_vip(_Config) ->
-  {ok, _} = update_de8b9dc86_marathon(),
-  timer:sleep(100),
-  [] = dcos_l4lb_lashup_vip_listener:lookup_vips([]),
-  [{ip, IP}] = dcos_l4lb_lashup_vip_listener:lookup_vips([{name, <<"de8b9dc86.marathon">>}]),
-  [{name, <<"de8b9dc86.marathon">>}] = dcos_l4lb_lashup_vip_listener:lookup_vips([{ip, IP}]),
-  ok.
-
-update_de8b9dc86_marathon() ->
-  lashup_kv:request_op(?VIPS_KEY2, {update, [{update,
-    {{tcp, {name, {<<"de8b9dc86">>, <<"marathon">>}}, 6000}, riak_dt_orswot},
-    {add, {{10, 0, 1, 31}, {{10, 0, 1, 31}, 12998}}}
-  }]}).
+-define(LOCALHOST, {127, 0, 0, 1}).
+resolve(DType, DName) ->
+    DNSQueries = [#dns_query{name=DName, type=DType}],
+    DNSMessage = #dns_message{
+        rd=true, qc=length(DNSQueries),
+        questions=DNSQueries
+    },
+    #dns_message{answers=DNSAnswers} =
+        erldns_handler:do_handle(DNSMessage, ?LOCALHOST),
+    DNSAnswers.
