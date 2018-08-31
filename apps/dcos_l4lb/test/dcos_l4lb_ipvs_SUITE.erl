c069f24b70ca3e8a26844c9654698327b2ffd98b
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-08-31 10:33:38 -0700

    Move test_normalize/1 to ipvs test suite

--- apps/dcos_l4lb/test/dcos_l4lb_ipvs_SUITE.erl
+++ apps/dcos_l4lb/test/dcos_l4lb_ipvs_SUITE.erl
@@ -15,14 +15,14 @@
 -export([
     all/0,
     init_per_testcase/2, end_per_testcase/2,
-    test_v4/1, test_v6/1
+    test_v4/1, test_v6/1, test_normalize/1
 ]).
 
 all() -> all(os:cmd("id -u"), os:getenv("CIRCLECI")).
 
 %% root tests
 all("0\n", false) ->
-    [test_v4, test_v6];
+    [test_v4, test_v6, test_normalize];
 
 %% non root tests
 all(_, _) -> [].
@@ -154,3 +154,67 @@ test_v6(Config) ->
     error = test_vip(Family, VIP),
     remove_vip(VIP),
     error = test_vip(Family, VIP).
+
+test_normalize(_Config) ->
+    Normalized =
+        normalize_services_and_dests({service(), [
+            destination(<<10, 10, 0, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>),
+            destination(<<10, 10, 0, 248, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>),
+            destination(<<10, 10, 0, 253, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>)
+        ]}),
+    Normalized =
+        {{tcp, {11, 197, 245, 133}, 9042}, [
+            {{10, 10, 0, 83}, 9042},
+            {{10, 10, 0, 248}, 9042},
+            {{10, 10, 0, 253}, 9042}
+        ]}.
+
+normalize_services_and_dests({Service0, Destinations0}) ->
+    {AddressFamily, Service1} = dcos_l4lb_ipvs_mgr:service_address(Service0),
+    Destinations1 = lists:map(
+                      fun(Dest) ->
+                          dcos_l4lb_ipvs_mgr:destination_address(AddressFamily, Dest)
+                      end, Destinations0),
+    Destinations2 = lists:usort(Destinations1),
+    {Service1, Destinations2}.
+
+service() ->
+    [
+        {address_family, 2},
+        {protocol, 6},
+        {address, <<11, 197, 245, 133, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>},
+        {port, 9042},
+        {sched_name, "wlc"},
+        {flags, 2, 4294967295},
+        {timeout, 0},
+        {netmask, 4294967295},
+        {stats, stats()}
+    ].
+
+destination(Address) ->
+    [
+        {address, Address},
+        {port, 9042},
+        {fwd_method, 0},
+        {weight, 1},
+        {u_threshold, 0},
+        {l_threshold, 0},
+        {active_conns, 0},
+        {inact_conns, 0},
+        {persist_conns, 0},
+        {stats, stats()}
+    ].
+
+stats() ->
+    [
+        {conns, 0},
+        {inpkts, 0},
+        {outpkts, 0},
+        {inbytes, 0},
+        {outbytes, 0},
+        {cps, 0},
+        {inpps, 0},
+        {outpps, 0},
+        {inbps, 0},
+        {outbps, 0}
+    ].
