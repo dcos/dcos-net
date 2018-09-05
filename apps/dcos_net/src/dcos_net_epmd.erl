4f448f9f4401c5ab6e53c74cd6903835abb7cb43
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-09-05 13:57:38 -0700

    Get rid of embedded EPMD server
    Embedded EPMD server was needed only for smooth upgrade from DC/OS
    1.10 to DC/OS 1.11. Erlang distribution works without EPMD server
    since DC/OS 1.11.

--- apps/dcos_net/src/dcos_net_epmd.erl
+++ apps/dcos_net/src/dcos_net_epmd.erl
@@ -1,17 +1,10 @@
 -module(dcos_net_epmd).
 
-% EPMD Module
 -export([
     register_node/3,
     address_please/3,
     port_please/2,
     names/1
-]).
-
-% EPMD Server
--export([
-    start_link/4,
-    init/4
 ]).
 
 -define(DIST_PROTO_VSN, 5).
@@ -48,58 +41,3 @@ port_please(_None, _Ip) ->
 port() ->
     {ok, Port} = dcos_net_app:dist_port(),
     Port.
-
-%%====================================================================
-%% EPMD Server
-%%====================================================================
-
--define(EPMD_NAMES_REQ, $n).
--define(EPMD_PORT2_RESP, $w).
--define(EPMD_PORT_PLEASE2_REQ, $z).
-
-start_link(Ref, Socket, Transport, Opts) ->
-    Pid = spawn_link(?MODULE, init, [Ref, Socket, Transport, Opts]),
-    {ok, Pid}.
-
-init(Ref, Socket, Transport, _Opts) ->
-    ok = ranch:accept_ack(Ref),
-    Transport:setopts(Socket, [{packet, 2}]),
-    case Transport:recv(Socket, 0, 5000) of
-        {ok, Data} ->
-            Data0 = handle_epmd(Data),
-            ok = Transport:setopts(Socket, [{packet, raw}]),
-            Transport:send(Socket, Data0);
-        {error, _Error} ->
-            ok
-    end,
-    ok = Transport:close(Socket).
-
-handle_epmd(<<?EPMD_NAMES_REQ>>) ->
-    Format = "name ~s at port ~w~n",
-    Data = [io_lib:format(Format, [Name, Port]) || {Name, Port} <- names()],
-    ServerPort = ranch:get_port(?MODULE),
-    [<<ServerPort:32>>, Data];
-handle_epmd(<<?EPMD_PORT_PLEASE2_REQ, Name/binary>>) ->
-    case lists:keymember(Name, 1, names()) of
-        true -> reply_port_epmd(Name);
-        false -> <<?EPMD_PORT2_RESP, 1>>
-    end;
-handle_epmd(_Data) ->
-    <<>>.
-
-reply_port_epmd(Name) ->
-    {ok, Port} = dcos_net_app:dist_port(),
-    NodeType = $M,
-    Protocol = 0,
-    HighVSN = ?DIST_PROTO_VSN,
-    LowVSN = ?DIST_PROTO_VSN,
-    Extra = <<>>,
-    <<?EPMD_PORT2_RESP, 0, Port:16, NodeType:8,
-      Protocol:8, HighVSN:16, LowVSN:16,
-      (byte_size(Name)):16, Name/binary,
-      (byte_size(Extra)):16, Extra/binary>>.
-
-names() ->
-    Hostname = dcos_net_dist:hostname(),
-    {ok, Nodes} = names(binary_to_list(Hostname)),
-    [{list_to_binary(Node), Port} || {Node, Port} <- Nodes].
