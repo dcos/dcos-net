e71752ada9f51f9ab6e8e43911b33e6a14854785
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-09-05 15:16:12 -0700

    Remove verification for ssl options.
    That verifications don't help us much and OTP 21 has much better
    logs for erlang distibution protocol over TCP/TLS. It also would be
    much easier to update tls configuration if we remove checks from
    the code. DC/OS integration tests already verify that DC/OS Net
    works fine with its TLS configuration.

--- apps/dcos_net/src/dcos_net_dist.erl
+++ apps/dcos_net/src/dcos_net_dist.erl
@@ -46,18 +46,14 @@ ssl_dist_opts() ->
             false
     end.
 
+%%====================================================================
+%% Dist functions
+%%====================================================================
 
 listen(Name) ->
     set_dist_port(Name),
     M = dist_module(),
-    try M:listen(Name) of
-        {ok, Result} ->
-            {ok, Result};
-        Other ->
-            critical_error("[~p] ~p", [Name, Other])
-    catch C:E ->
-        critical_error("[~p] ~p:~p", [Name, C, E])
-    end.
+    M:listen(Name).
 
 select(Node) ->
     M = dist_module(),
@@ -88,26 +84,16 @@ childspecs() ->
 %%====================================================================
 
 set_dist_port(navstar) ->
-    case dcos_net_app:dist_port() of
-        {ok, Port} ->
-            ok = application:set_env(kernel, inet_dist_listen_min, Port),
-            ok = application:set_env(kernel, inet_dist_listen_max, Port);
-        {error, Error} ->
-            critical_error("Couldn't find dist port [~p]", [Error])
-    end;
+    {ok, Port} = dcos_net_app:dist_port(),
+    ok = application:set_env(kernel, inet_dist_listen_min, Port),
+    ok = application:set_env(kernel, inet_dist_listen_max, Port);
 set_dist_port(_Name) ->
     ok.
 
 dist_module() ->
     case init:get_argument(ssl_dist_optfile) of
-        {ok, [[SSLDistOptFile]]} ->
-            try ssl_dist_opts(SSLDistOptFile) of
-                DistOpts ->
-                    lists:foreach(fun check_ssl_dist_opt/1, DistOpts),
-                    inet_tls_dist
-            catch T:E ->
-                critical_error("ssl_dist_opts (~s) [~p] ~p", [SSLDistOptFile, T, E])
-            end;
+        {ok, [_SSLDistOptFiles]} ->
+            inet_tls_dist;
         error ->
             inet_tcp_dist
     end.
@@ -118,42 +104,3 @@ ssl_dist_opts(SSLDistOptFile) ->
     catch error:badarg ->
         ssl_dist_sup:consult(SSLDistOptFile)
     end.
-
-check_ssl_dist_opt({Type, Opts}) ->
-    Checks = #{
-        verify => fun is_verify_peer/1,
-        secure_renegotiate => fun is_true/1,
-        fail_if_no_peer_cert => fun is_true/1,
-        certfile => fun is_file/1,
-        keyfile => fun is_file/1,
-        cacertfile => fun is_file/1,
-        depth => fun is_integer/1
-    },
-    lists:foreach(fun ({Key, Value}) ->
-        Fun = maps:get(Key, Checks, fun is_unexpected/1),
-        case Fun(Value) of
-            false ->
-                critical_error(
-                    "Unexpected value [~p ~p] ~p",
-                    [Type, Key, Value]);
-            true -> ok
-        end
-    end, Opts).
-
-is_file(File) ->
-    filelib:is_file(File, erl_prim_loader).
-
-is_verify_peer(Value) ->
-     Value =:= verify_peer.
-
-is_true(Value) ->
-    Value =:= true.
-
-is_unexpected(_Value) ->
-    false.
-
-critical_error(Format, Args) ->
-    io:format(standard_error, "~p: ", [?MODULE]),
-    io:format(standard_error, Format, Args),
-    io:format(standard_error, "~n", []),
-    erlang:halt(1).
