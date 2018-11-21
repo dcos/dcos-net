50c9d932c23515ace02901d84867838b811b9d66
Sergey Urbanovich <sergey.urbanovich@gmail.com>
2018-11-20 20:37:04 -0800

    Add dcos_net_utils:system/2

--- apps/dcos_net/src/dcos_net_utils.erl
+++ apps/dcos_net/src/dcos_net_utils.erl
@@ -5,7 +5,9 @@
 -endif.
 
 -export([
-    complement/2
+    complement/2,
+    system/1,
+    system/2
 ]).
 
 %%%===================================================================
@@ -46,5 +48,60 @@ complement_test() ->
     ?assertEqual(
         {[a, b, c], [d, e, f]},
         {lists:sort(A), lists:sort(B)}).
+
+-endif.
+
+%%%===================================================================
+%%% System functions
+%%%===================================================================
+
+-spec(system([binary()]) ->
+    {ok, binary()} | {error, atom() | {exit_status, non_neg_integer()}}).
+system(Command) ->
+    system(Command, 5000).
+
+-spec(system([binary()], non_neg_integer()) ->
+    {ok, binary()} | {error, atom() | {exit_status, non_neg_integer()}}).
+system([Command|Args], Timeout) ->
+    Opts = [exit_status, binary, {args, Args}],
+    EndTime = erlang:monotonic_time(millisecond) + Timeout,
+    try erlang:open_port({spawn_executable, Command}, Opts) of
+        Port ->
+            system_loop(Port, EndTime, <<>>)
+    catch error:Error ->
+        {error, Error}
+    end.
+
+-spec(system_loop(port(), integer(), binary()) ->
+    {ok, binary()} | {error, atom() | {exit_status, non_neg_integer()}}).
+system_loop(Port, EndTime, Acc) ->
+    Timeout = EndTime - erlang:monotonic_time(millisecond),
+    receive
+        {Port, {data, Data}} ->
+            system_loop(Port, EndTime, <<Acc/binary, Data/binary>>);
+        {Port, {exit_status, 0}} ->
+            {ok, Acc};
+        {Port, {exit_status, ExitCode}} ->
+            {error, {exit_status, ExitCode}}
+    after Timeout ->
+        erlang:port_close(Port),
+        {error, timeout}
+    end.
+
+-ifdef(TEST).
+
+system_test() ->
+    case system([<<"/bin/pwd">>]) of
+        {error, enoent} -> ok;
+        {ok, Pwd} ->
+            {ok, Cwd} = file:get_cwd(),
+            ?assertEqual(iolist_to_binary([Cwd, "\n"]), Pwd)
+    end.
+
+system_timeout_test() ->
+    case system([<<"/bin/dd">>], 100) of
+        {error, enoent} -> ok;
+        {error, timeout} -> ok
+    end.
 
 -endif.
