ca3e0f7fc25b3a3f344372dbc8a1d6d3d4642562
Jason Kölker <jason@koelker.net>
2020-02-19 09:54:30 +0000

    net: add killing state

--- apps/dcos_net/src/dcos_net_mesos_listener.erl
+++ apps/dcos_net/src/dcos_net_mesos_listener.erl
@@ -30,7 +30,7 @@
     ports => [task_port()]
 }.
 -type runtime() :: docker | mesos | unknown.
--type task_state() :: preparing | running | terminal.
+-type task_state() :: preparing | running | killing | terminal.
 -type task_port() :: #{
     name => binary(),
     host_port => inet:port_number(),
@@ -441,6 +441,8 @@ handle_task_state(TaskObj, _Task) ->
             terminal;
         <<"TASK_RUNNING">> ->
             running;
+        <<"TASK_KILLING">> ->
+            killing;
         _TaskState ->
             preparing
     end.
